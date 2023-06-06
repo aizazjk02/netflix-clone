4dfb85e35657995a7f8bcb6db442ba2c9bac32da
aizaz <56686849+aizazjk02@users.noreply.github.com>
2023-06-06 20:04:26 +0530

    chore : Link app with the firebase.
    Add config to the firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCRXMuMN9DMmjRE6NTy5UjuF4cWVQ1J_cw",
+    authDomain: "netflix-clone-f0f4c.firebaseapp.com",
+    projectId: "netflix-clone-f0f4c",
+    storageBucket: "netflix-clone-f0f4c.appspot.com",
+    messagingSenderId: "935410719612",
+    appId: "1:935410719612:web:634ce8327c579c831e62f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
